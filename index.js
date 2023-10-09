51ddb556968f7e47c7fda4ddf24a2feb44c41b53
WVallejos <guille.vallejosr@gmail.com>
2023-10-09 15:45:48 -0300

    getWilly

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ function laCajaDePandora(numero){
     // proximamente escribiremos codigo aqui
     return numero % 2 === 0 ? numero.toString(2) : numero.toString(6)
     }
+
+function getWilly(){
+    return {
+        name: 'Guillermo',
+        age: 32,
+        nationality: 'Argentino'
+    }
+}
+
+console.log(getWilly());
