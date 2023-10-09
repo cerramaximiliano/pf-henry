82c61d69f020b1d9f33bb7491da0297700f6347e
Juan Leonel Pogonza <leonelpogonza91@gmail.com>
2023-10-09 15:34:27 -0300

    Funcion CajaDePandora

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+function laCajaDePandora(numero) {
+  if (Number.isInteger(numero)) {
+    if (numero % 2 === 0) {
+      // Número par, convertir a binario
+      return numero.toString(2);
+    } else {
+      // Número impar, convertir a hexadecimal
+      return numero.toString(16);
     }
+  } else {
+    return 'Debe ingresar un número entero.';
+  }
+}
