fbce19d15029af8cb33677f49eec9421624758e9
Lucas Blanco <126288618+FixGlitch@users.noreply.github.com>
2023-10-09 15:43:00 -0300

    tuki

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+function laCajaDePandora(numero) {
+    if (typeof numero !== 'number') {
+      return 'Por favor, ingresa un número válido.';
     }
+  
+    if (numero % 2 === 0) {
+      return numero.toString(2);
+    } else {
+      return numero.toString(16);
+    }
+  }
