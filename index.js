cbf90665e2e86e4e223f81e94363303e4b8b972c
Maximiliano Cerra <cerramaximiliano@gmail.com>
2023-10-09 15:45:59 -0300

    maxi function

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function laCajaDePandora(numero){
         if(numero%2 === 0  ) return Number(numero).toString(2)
         else return Number(numero).toString(16) 
 }
+
+function getMaxi (  ) {
+    return {
+        nombre: 'Maxi',
+        edad: 29,
+        nacionalidad: 'argentino'
+    }
+};
