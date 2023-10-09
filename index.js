e03a41b762ac76aff3a1fcfb49160896a9ad0650
Maximiliano Cerra <65555679+cerramaximiliano@users.noreply.github.com>
2023-10-09 15:35:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 function laCajaDePandora(numero){
     // proximamente escribiremos codigo aqui
-    }
+        if(numero%2 === 0  ) return Number(numero).toString(2)
+        else return Number(numero).toString(16) 
+}
