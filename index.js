458e2860012e5893f88786c3e8e6ca34c5dcda97
Mateo Ossorio Arana <matossoara@gmail.com>
2023-10-09 15:47:06 -0300

    function added mateo

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 function laCajaDePandora(numero){
     if( numero % 2 === 0 ){
         return numero.toString(2)
-    }
+        }
     else {
         return numero.toString(16)
+        }
     }
-    }
+
+function mateo(){
+    let nombre = {
+        nombre: "mateo",
+        edad: 25,
+        nacionalidad: "argentina"
+    };
+    return nombre
+}
