fa64986b73f79c17e3fc1affe66b28b521cadaf7
JuanUbaldi <106642655+JuanUbaldi@users.noreply.github.com>
2023-10-09 15:41:11 -0300

    commit juan

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
-    }
+
+
+function laCajaDePandora(numero) {
+  if (numero % 2 === 0) {
+   
+    return numero.toString(2);
+  } else {
+   
+    return numero.toString(16);
+  }
+}
