66c359f0cf394375b8c165eb8e43349676277ef1
JuanUbaldi <106642655+JuanUbaldi@users.noreply.github.com>
2023-10-09 15:46:10 -0300

    commit2

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-
-
 function laCajaDePandora(numero) {
   if (numero % 2 === 0) {
-   
     return numero.toString(2);
   } else {
-   
     return numero.toString(16);
   }
+}
+
+function juanUbaldi() {
+  return {
+    name: "juan ubaldi",
+    nacionalidad: "Argentino",
+    edad: "31",
+  };
 }
