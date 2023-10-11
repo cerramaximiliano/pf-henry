8904c163b41241ea7fa173605a3c4143c1b208d2
WVallejos <guille.vallejosr@gmail.com>
2023-10-11 17:02:19 -0300

    routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const {createUser, getUser} = require('../handlers/usersHndlr');
 const router = express.Router();
+
 
 
 router.get('/', (req,res) => {
     console.log('Get All Users')
     res.send(`Users route`)
 });
+
+router.post('/register', createUser)
+router.get('/:id', getUser)
 
 module.exports = router;
