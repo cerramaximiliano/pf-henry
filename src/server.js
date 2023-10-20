a89a226533ce4c492e848ea2a7ddb731c235f52f
WVallejos <guille.vallejosr@gmail.com>
2023-10-20 13:19:28 -0300

    userModels-create

--- src/server.js
+++ src/server.js
@@ -8,8 +8,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 const server = express();
 
-server.use(morgan("dev"));
 server.use(express.json({limit: '50mb'}));
+server.use(morgan("dev"));
 server.use(express.urlencoded({extended: false}, {limit: '50mb'}));
 server.use(cors());
 
