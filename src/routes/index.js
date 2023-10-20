5f9e460a3ca5fb411392e757369ac7b346726880
cerramaximiliano <cerramaximiliano@gmail.com>
2023-10-20 18:24:16 -0300

    feature/stripeRoute

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const usersRoutes = require('./users');
 const productsRoutes = require('./products');
 const imagesRoutes = require('./images');
+const stripeRoutes = require('./checkout');
 
 router.use('/users', usersRoutes);
 router.use('/products', productsRoutes);
 router.use('/upload', imagesRoutes);
+router.use('/checkout', stripeRoutes)
 
 module.exports = router;
 
