1cc630226eab03dbbc37b0573206be63e571bbc9
cerramaximiliano <cerramaximiliano@gmail.com>
2023-10-20 18:36:01 -0300

    fix/routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const usersRoutes = require('./users');
 const productsRoutes = require('./products');
 const imagesRoutes = require('./images');
-const stripeRoutes = require('./checkout');
 
 router.use('/users', usersRoutes);
 router.use('/products', productsRoutes);
