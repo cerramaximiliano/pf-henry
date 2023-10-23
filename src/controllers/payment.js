b14c31ab516086061dda0a593d8f914e490419c4
JuanUbaldi <106642655+JuanUbaldi@users.noreply.github.com>
2023-10-23 15:15:25 -0300

    commit back

--- src/controllers/payment.js
+++ src/controllers/payment.js
@@ -1,37 +1,40 @@
-const Stripe = require('stripe');
+const Stripe = require("stripe");
 const STRIPE_KEY = process.env.STRIPE_KEY;
 const stripe = new Stripe(STRIPE_KEY);
 
+console.log(STRIPE_KEY);
+
 const createSession = async (req, res) => {
-    let { products, total_price } = req.body;
+  let { products, totalPrice } = req.body;
 
-    if (typeof total_price !== 'number' && Array.isArray(products)) {
-        total_price = products.reduce((acc, product) => acc + product.price, 0);
-    }
+  if (typeof totalPrice !== "number" && Array.isArray(products)) {
+    totalPrice = products.reduce((acc, product) => acc + product.price, 0);
+  }
 
-    const lineItems = products.map(product => {
-        return {
-            price_data: {
-                product_data: {
-                    name: product.title,
-                    images: [product.image],
-                },
-                currency: 'usd',
-                unit_amount: product.price * 100, // Convert price to cents
-            },
-            quantity: 1
-        };
-    });
+  const lineItems = products.map((product) => {
+    return {
+      price_data: {
+        product_data: {
+          name: product.title,
+          images: [product.image],
+        },
+        currency: "usd",
+        unit_amount: product.price * 100, // Convert price to cents
+      },
+      quantity: 1,
+    };
+  });
 
-    const session = await stripe.checkout.sessions.create({
-        line_items: lineItems,
-        mode: 'payment',
-        success_url: 'http://localhost:3001/payment/success',
-        cancel_url: 'http://localhost:3001/payment/cancel'
-    });
-    
-    res.json(session.url);
+  const session = await stripe.checkout.sessions.create({
+    line_items: lineItems,
+    mode: "payment",
+    success_url: "http://localhost:3001/products",
+    cancel_url: "http://localhost:3001/payment/cancel",
+  });
+  console.log(session.url);
+
+  res.json(session.url);
+  console.log(req.body);
 };
 
 module.exports = createSession;
-
