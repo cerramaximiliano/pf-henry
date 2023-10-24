55f74a6766656bc94d5c2a6d0cb3df3e96e9a818
cerramaximiliano <cerramaximiliano@gmail.com>
2023-10-24 13:30:03 -0300

    fix/createOrderController

--- src/controllers/payment.js
+++ src/controllers/payment.js
@@ -2,33 +2,13 @@ const Stripe = require("stripe");
 const STRIPE_KEY = process.env.STRIPE_KEY;
 const stripe = new Stripe(STRIPE_KEY);
 const Order = require('../models/orders');
-
-const URL_BASE = 'http://localhost:5173/';
-
-
-const createOrder = async (result) => {
-  console.log(result)
-  console.log(result.products)
-  try {
-      const newOrder = new Order({
-        products:  result.products,
-        total: result.totalPrice,
-        userId: 12345687,
-        status:"pending"
-      });
-      await newOrder.save();
-      return { newOrder };
-  } catch (error) {
-    throw new Error(error);
-  }
-};
+const createOrder = require('../controllers/createOrder');
 
 
 const createSession = async (req, res) => {
   let { products, totalPrice } = req.body;
 
-  const newOrder = await createOrder(products, totalPrice);
-  console.log(newOrder);
+  const newOrder = await createOrder({products, totalPrice});
 
   if (typeof totalPrice !== "number" && Array.isArray(products)) {
     totalPrice = products.reduce((acc, product) => acc + product.price, 0);
@@ -51,13 +31,10 @@ const createSession = async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     line_items: lineItems,
     mode: "payment",
-    success_url: `http://localhost:5173/myaccount/${newOrder._id}`,
-    cancel_url: "http://localhost:3001/payment/cancel",
+    success_url: `${process.env.URL_FRONT}/myaccount/${newOrder._id}`,
+    cancel_url: `${process.env.URL_FRONT}/myaccount/error`,
   });
-  console.log(session.url);
-
   res.json(session.url);
-  console.log(req.body);
 };
 
 module.exports = createSession;
