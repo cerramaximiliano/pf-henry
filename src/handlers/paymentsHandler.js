e8f245ba950e8c703167bca3fb4af0e3158f9305
Maximiliano Cerra <cerramaximiliano@gmail.com>
2023-11-02 06:54:11 -0300

    fix/update payment

--- src/handlers/paymentsHandler.js
+++ src/handlers/paymentsHandler.js
@@ -7,7 +7,6 @@ const {createOrder} = require('../controllers/ordersControllers');
 
 const createSession = async (req, res) => {
   let { products, totalPrice, userId } = req.body;
-  console.log('Products', products)
   const newOrder = await createOrder({products, totalPrice, userId});
   if (typeof totalPrice !== "number" && Array.isArray(products)) {
     totalPrice = products.reduce((acc, product) => acc + product.price, 0);
@@ -35,4 +34,35 @@ const createSession = async (req, res) => {
   res.json(session.url);
 };
 
-module.exports = createSession;
+const updateSession = async (req, res) => {
+  let { id } = req.body;
+  try {
+    const order = await Order.findById(id);
+    const lineItems = order.products.map((product) => {
+      return {
+        price_data: {
+          product_data: {
+            name: product.title,
+            images: [product.image],
+          },
+          currency: "usd",
+          unit_amount: Math.round(product.price * 100 * 100)/100,
+        },
+        quantity: product.quantity,
+      };
+    });
+  
+    const session = await stripe.checkout.sessions.create({
+      line_items: lineItems,
+      mode: "payment",
+      success_url: `${process.env.URL_FRONT}/myaccount/orders/?id=${order._id}`,
+      cancel_url: `${process.env.URL_FRONT}/myaccount/orders/?id=${order._id}&error=true`,
+    });
+    res.json(session.url);
+  }
+  catch(err){
+    res.status(500).json({ok:false, error: err.message})
+  }
+};
+
+module.exports = {createSession, updateSession};
--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const createSession = require('../handlers/paymentsHandler')
-// const webHookHandler = require('../handlers/webHookHandler')
+const {createSession, updateSession} = require('../handlers/paymentsHandler')
 
 router.post('/create-checkout-session', createSession);
-
-router.get('/success', (req,res) => {res.send(`success`)});
-
-// router.post('/webhook', webHookHandler);
-// router.get('/webhook', webHookHandler);
-
-router.get('/cancel', (req,res) => {res.send(`cancel`)});
+router.post('/update/session', updateSession)
 
 
 module.exports = router;
